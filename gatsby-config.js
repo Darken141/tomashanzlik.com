3ad1872826aa9dd7c916d4c9443d0a00d8241a60
Darken141 <47373497+Darken141@users.noreply.github.com>
2019-06-06 05:03:04 +0100

    initial page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `VideoProdukcia Tomáš Hanzlík`,
+    description: `Videoprodukcia firemných eventov, svadobných videi, reklamnych spotov a videoklipov`,
+    author: `@TomasHanzlik`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ikona.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
