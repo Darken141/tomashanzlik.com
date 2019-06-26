15e145cd4ce14a98b289b0e21118fe40d6fbc74e
Darken141 <47373497+Darken141@users.noreply.github.com>
2019-06-26 02:18:34 +0100

    add analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import React from "react"
 import SEO from "../components/seo"
+import ReactGA from 'react-ga';
 
 import Productions from '../main/components/Productions';
 import Counter from '../main/components/Counter';
 import '../main/main.css';
 
 import './index.css';
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-142792433-1');
+  ReactGA.pageview('/homepage');
+}
+
+
 
 const IndexPage = () => (
   <div className="App">
